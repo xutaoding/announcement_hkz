839ceb55c0b849a0ff82372c100c23e6431ccc11
xutao.ding <xutao.ding@chinascopefinancial.com>
2015-12-15 16:44:04 +0800

    add index

--- hkz/third_update.py
+++ hkz/third_update.py
@@ -115,14 +115,14 @@ def third_update():
                 except Exception as e:
                     print 'Error:', e.message
 
-                    # 创建索引
-                    inds_mon = coll_in.get({'sid': url})
-                    ind_url = "http://192.168.250.205:17081/indexer/services/indexes/delta.json?" \
-                              "indexer=announce_hkz&taskids="
-                    if inds_mon:
-                        jdata = BaseDownloadHtml().get_html(ind_url + str(inds_mon['_id']))[0]
-                        if json.loads(jdata)['code'] == 200:
-                            print '\tcreate index is ok!\n\n'
+                # 创建索引
+                inds_mon = coll_in.get({'sid': url})
+                ind_url = "http://192.168.250.205:17081/indexer/services/indexes/delta.json?" \
+                          "indexer=announce_hkz&taskids="
+                if inds_mon:
+                    jdata = BaseDownloadHtml().get_html(ind_url + str(inds_mon['_id']))[0]
+                    if json.loads(jdata)['code'] == 200:
+                        print '\tcreate index is ok!\n\n'
     coll_in.disconnect()
     coll_cat.disconnect()
     coll_secu.disconnect()
